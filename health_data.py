04c00e269864afd97c7609cded1b7662414f0b7e
Shahmeera111 <mahmoodrehman5@gmail.com>
2026-09-03 23:45:01 -0700

    Update health_data.py

--- health_data.py
+++ health_data.py
@@ -575,7 +575,7 @@ comparison = pd.DataFrame({
 })
 
 print("FIRST 10 DAYS")
-display(comparison.head(10))
+st.dataframe(comparison.head(10))
 
 print("\nLAST 10 DAYS")
 display(comparison.tail(10))
